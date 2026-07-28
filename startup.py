66e8633fb8250afa3f3773f278b7ea74a2bf8b8b
ruiming2ai <ruiming2ai@outlook.com>
2026-07-27 17:25:19 -0700

    "Main" - Rename Repo & Tool
    (cherry picked from commit 1e5612c7baed4bcc3491c8b5df8c829b330e216d)

---
+++ startup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""EasyBIM extension startup automation."""
+
+
+try:
+    from easybim import coordination_review_passive
+    coordination_review_passive.register_passive_detector()
+except Exception:
+    pass
+
+
+try:
+    from easybim import auto_update
+
+    if not auto_update.should_skip_startup(auto_update.get_startup_guard_state()):
+        auto_update.mark_startup_attempted()
+        auto_update.run_startup_auto_update()
+except Exception:
+    pass
