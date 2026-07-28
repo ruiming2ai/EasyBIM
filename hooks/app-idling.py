b0fa17657e73103c649cd0b906395ed89ac5f380
RuimingLiu <142599989+ruiming2ai@users.noreply.github.com>
2026-07-27 17:25:19 -0700

    "Main" - Rename Repo & Tool
    (cherry picked from commit 1e5612c7baed4bcc3491c8b5df8c829b330e216d)

--- hooks/app-idling.py
+++ hooks/app-idling.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
-"""Process RL Tools startup queue on each Revit idling event."""
+"""Process EasyBIM startup queue, temp-phase runtime, and close stop."""
 
-from rltools.messages import process_startup_jobs
+from easybim.messages import process_startup_jobs
+
+try:
+    from easybim import temp_phase_view
+except Exception:
+    temp_phase_view = None
+
+try:
+    from easybim import close_stop
+except Exception:
+    close_stop = None
+
+try:
+    _EVENT_ARGS = EXEC_PARAMS.event_args
+except Exception:
+    _EVENT_ARGS = None
 
 try:
     process_startup_jobs()
